0459f883d7ce803c5ec316c18efc35397fdeabd2
SuvrangshuBarua <suvrangshu.turno@gmail.com>
2025-05-03 00:11:35 +0200

    ladok login with user credentials added

--- main.py
+++ main.py
@@ -7,21 +7,97 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, ElementNotInteractableException, ElementClickInterceptedException
 
+# constants
+ladok_url = "https://www.student.ladok.se/"
+timeedit_url = "https://cloud.timeedit.net/chalmers/web/public/"
+wait_time = 2  
+
+def get_user_credentials():
+    username = input("Enter your username: ").strip()
+    if not username:
+        raise ValueError("Username cannot be empty.")
+    password = input("Enter your password: ").strip()
+    if not password:
+        raise ValueError("Password cannot be empty.")
+    return username, password
+
+def ladok_login(driver, username, password):
+    try:
+        driver.get(ladok_url)
+
+        WebDriverWait(driver, wait_time).until(
+            EC.presence_of_element_located((By.ID, "discoveryServiceIdBtn"))
+        ).click()
+       
+        uni_search_box = WebDriverWait(driver, wait_time).until(
+            EC.element_to_be_clickable((By.ID, "searchinput"))
+        )
+        uni_search_box.click()
+        uni_search_box.clear()
+        uni_search_box.send_keys("University of Gothenburg" + Keys.ENTER)
+
+        WebDriverWait(driver, wait_time).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "text-truncate.label.primary"))
+        ).click()
+
+        username_input = WebDriverWait(driver, wait_time).until(
+            EC.presence_of_element_located((By.ID, "userNameInput"))
+        )
+
+        username_input.click()
+        username_input.clear()
+        username_input.send_keys(username + Keys.ENTER) 
+
+        password_input = WebDriverWait(driver, wait_time).until(
+            EC.presence_of_element_located((By.ID, "passwordInput"))
+        )
+        password_input.click()
+        password_input.clear()
+        password_input.send_keys(password + Keys.ENTER)
+        print("Entered username and password.")
+        return True
+    except (TimeoutException, ElementNotInteractableException, ElementClickInterceptedException) as e:
+        print(f"Error occurred while clicking the link: {e}")
+        return False
+    
+def extract_course_codes(driver):
+    try:
+        course_cards = WebDriverWait(driver, wait_time).until(
+            EC.presence_of_all_elements_located((By.TAG_NAME, "ladok-aktuell-kurs-kort"))
+        )
+
+        course_codes = []
+
+        for index, course_card in enumerate(course_cards):
+            try:
+                course_name_element = course_card.find_element(By.CSS_SELECTOR, "h3.card-title span.ladok-card-body-rubrik a")
+                course_name = course_name_element.text.strip()
+                course_code = course_name.split(" - ")[-1].strip()
+                course_codes.append(course_code)
+            except Exception as e:
+                print(f"Error occurred while extracting course code from card {index}: {e}")
+                continue
+
+        return course_codes
+
+    except (TimeoutException, ElementNotInteractableException) as e:
+        print(f"Error occurred while locating course cards: {e}")
+        return []
+    
 def timeedit_chalmers(driver, courses):
     try:
-        element = WebDriverWait(driver, 2).until(
+        driver.get(timeedit_url)
+        WebDriverWait(driver, wait_time).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, ".leftlistcolumn > a:last-child"))
-        )
-        element.click()
-        print("Clicked on Chalmers University of Technology link.")
+        ).click()
+
         for course in courses:
             search_course(driver, course)
 
-        objectbasketgo_button = WebDriverWait(driver, 2).until(
+        WebDriverWait(driver, wait_time).until(
             EC.element_to_be_clickable((By.ID, "objectbasketgo"))
-        )
-        objectbasketgo_button.click()
-        print("Clicked on the objectbasketgo button.")
+        ).click()
+
     except (TimeoutException, ElementNotInteractableException, ElementClickInterceptedException) as e:
         print(f"Error occurred while clicking the link: {e}")
         return        
@@ -29,7 +105,7 @@ def timeedit_chalmers(driver, courses):
     
 def search_course(driver, course):
     try:
-        search_box = WebDriverWait(driver, 2).until(
+        search_box = WebDriverWait(driver, wait_time).until(
             EC.presence_of_element_located((By.ID, "ffsearchname"))
         )
         search_box.click()
@@ -37,14 +113,14 @@ def search_course(driver, course):
         search_box.send_keys(course + Keys.ENTER)
         print("Found course:", course)
 
-        add_all_button = course_item = WebDriverWait(driver, 2).until(
+        add_all_button = course_item = WebDriverWait(driver, wait_time).until(
             EC.presence_of_element_located((By.XPATH, f"//div[contains(@class, 'clickable2') and contains(normalize-space(), '{course}')]"))
         )
         add_all_button.click()
 
         print("Added course:", course)
 
-        WebDriverWait(driver, 2).until(
+        WebDriverWait(driver, wait_time).until(
             EC.invisibility_of_element_located((By.XPATH, f"//div[contains(@class, 'clickable2') and contains(normalize-space(), '{course}')]"))
         )
 
@@ -52,20 +128,27 @@ def search_course(driver, course):
         print(f"Error occurred while searching for course: {e}")
         return
 
+def main():
+    service = Service(executable_path="chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
 
-service = Service(executable_path="chromedriver.exe")
-driver = webdriver.Chrome(service=service)
-course_input = input("Enter the course codes separated by semicolon: ").strip().upper()
-courses = [code.strip() for code in course_input.split(";") if code.strip()]
+    try:
+        username, password = get_user_credentials()
+        
+        if ladok_login(driver, username, password):
+            extract_course_codes(driver)
+            courses = extract_course_codes(driver)
+            if courses:
+                timeedit_chalmers(driver, courses)
+            else:
+                print("No courses found to process.")
+    finally:
+        # Take a screenshot before closing the driver
+        driver.save_screenshot("final_result.png")
+        print("Screenshot saved as final_result.png")
+        time.sleep(2)
+        driver.quit()
+        print("Driver closed successfully.")
 
-try:
-
-    driver.get("https://cloud.timeedit.net/chalmers/web/public/")
-    timeedit_chalmers(driver, courses)
-finally:
-    # Take a screenshot before closing the driver
-    driver.save_screenshot("final_result.png")
-    print("Screenshot saved as final_result.png")
-    time.sleep(10)
-    driver.quit()
-    print("Driver closed successfully.")
+if __name__ == "__main__":
+    main()
